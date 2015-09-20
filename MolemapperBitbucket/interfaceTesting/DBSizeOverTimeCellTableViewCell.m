f89937faf9b0a39f4c860cf1f8370f3ee9bf6555
Karpacs Istvan <karpacsi@gmail.com>
2015-09-20 18:37:42 +0200

    more float fix

--- MolemapperBitbucket/interfaceTesting/DBSizeOverTimeCellTableViewCell.m
+++ MolemapperBitbucket/interfaceTesting/DBSizeOverTimeCellTableViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "DBSizeOverTimeCellTableViewCell.h"
+#import "Measurement.h"
 #import "DashboardModel.h"
 
 @implementation DBSizeOverTimeCellTableViewCell
@@ -23,11 +24,33 @@
     _moleNameLabel.text = [mole objectForKey:@"name"];
     NSNumber* size = [mole objectForKey:@"size"];
     NSNumber* percentChange = [mole objectForKey:@"percentChange"];
+    //Measurement* measurement = [mole objectForKey:@"measurement"];
+    //NSDate* date = measurement.date;
     
-    _moleSizeLabel.text = [NSString stringWithFormat:@"%2.1f%% mm", [size floatValue]];
-    _moleProgressLabel.text = [NSString stringWithFormat:@"%2.1f%%", [percentChange floatValue]];
+    _moleSizeLabel.text = [NSString stringWithFormat:@"%2.1f mm, April 12 2087", [size floatValue]];
+    _moleProgressLabel.text = [percentChange floatValue] > 0.0f ? [NSString stringWithFormat:@"%2.1f%%", [percentChange floatValue]] : @"0.0%%";
     
     // Configure the view for the selected state
+}
+
+- (NSString*) getFormatedDate
+{
+    NSNumber* lastDate = [[DashboardModel sharedInstance] daysUntilNextMeasurementPeriod];//[_dbModel daysSinceLastFollowup];
+    
+    NSDate* date = [NSDate date];
+    
+    NSDateComponents* comps = [[NSDateComponents alloc]init];
+    comps.day = -lastDate.integerValue;
+    
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    
+    NSDate* correctedDay = [calendar dateByAddingComponents:comps toDate:date options:nil];
+    
+    NSDateFormatter* formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"MMMM dd";
+    NSString* dateString = [formatter stringFromDate:correctedDay];
+    
+    return dateString;
 }
 
 @end
