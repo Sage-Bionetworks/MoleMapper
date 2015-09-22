5108374dedafcc273f2fe5de5b0e473b216c41d6
Karpacs Istvan <karpacsi@gmail.com>
2015-09-22 10:18:34 +0200

    uv exposure view controller added with no content and biggest mole cell conversion is fixed

---
+++ MolemapperBitbucket/interfaceTesting/DashboardUVExposure.h
@@ -0,0 +1,12 @@
+//
+//  DashboardUVExposure.h
+//  MoleMapper
+//
+//  Created by Karpács István on 21/09/15.
+//  Copyright © 2015 Webster Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardUVExposure : UITableViewCell
+@end
---
+++ MolemapperBitbucket/interfaceTesting/DashboardUVExposure.m
@@ -0,0 +1,24 @@
+//
+//  DashboardUVExposure.m
+//  MoleMapper
+//
+//  Created by Karpács István on 21/09/15.
+//  Copyright © 2015 Webster Apps. All rights reserved.
+//
+
+#import "DashboardUVExposure.h"
+
+@implementation DashboardUVExposure
+
+- (void)awakeFromNib {
+    // Initialization code
+    Heal
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
